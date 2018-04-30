43a44793a92dd62ce127531851b55cdcfb3d077a
Julia Shreckhise <julia.shreckhise@gmail.com>
2018-04-29 22:29:00 -0400

    need form validation, questions, and modal functionality

---
+++ app/data/friends.js
@@ -0,0 +1,37 @@
+var friends = [
+    {
+        name: "Julia",
+        photo: "http://www.zarias.com/wp-content/uploads/2015/12/61-cute-puppies.jpg",
+        scores: [
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1
+        ]
+    },
+    {
+        name:"Ahmed",
+        photo:"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        scores:[
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+          ]
+      } 
+];
+
+module.exports = friends;
+
---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,54 @@
+var friends = require("../data/friends");
+
+
+module.exports = function (app) {
+    // used to display a JSON of all possible friends
+    app.get("/api/friends", (req, res) => {
+        res.json(friends);
+    });
+
+    // used to handle incoming survey results & compatibility logic.
+    app.post("/api/friends", (req, res) => {
+
+        var newScores = req.body.scores;
+
+        var bestMatch = [{ diff: 1000 }];
+
+        //go through array of all the friends
+        friends.forEach((friend) => {
+            var diff = 0;
+            //go through the newScores for each friend and tally the differences the between each answer
+            friend.scores.forEach((score, index) => {
+                diff += Math.abs(score - newScores[index]);
+            });
+
+            //go through bestMatch array and current friend is a better match (diff is less), replace that friend
+            //if current friend is equally as good of a match, add it to bestMatch
+            bestMatch.forEach((match, index) => {
+                if (match.diff > diff) {
+                    bestMatch.splice(index, 1, {
+                        name: friend.name,
+                        photo: friend.photo,
+                        diff: diff
+                    });
+                }
+                else if (match.diff === diff) {
+                    bestMatch.push(
+                        {
+                            name: friend.name,
+                            photo: friend.photo,
+                            diff: diff
+                        }
+                    );
+                }
+            });
+        });
+
+        res.json(bestMatch);
+        // Once you've found the current user's most compatible friend, display the result as a modal pop-up.
+        // The modal should display both the name and picture of the closest match.
+        console.log("bestMatch array at index 0 " + bestMatch[0].name);
+    });
+}
+
+
